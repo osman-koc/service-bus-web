3650b3e453099b57a7cab092eaf5b6c95654a1ed
Osman Koç <info@osmkoc.com>
2020-11-30 00:01:35 +0300

    Add sbadmin template files, razor pages (Error page)

---
+++ src/ServiceBusWeb/Pages/Error.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+using System.IO;
+
+namespace ServiceBusWeb.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class ErrorModel : PageModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string Referrer { get; set; }
+        public bool ShowReferrer => !string.IsNullOrEmpty(Referrer);
+
+        public string ExceptionMessage { get; set; }
+        public bool ShowExceptionMessage => !string.IsNullOrEmpty(ExceptionMessage);
+
+        public string ErrorStatusCode { get; set; }
+        public string OriginalURL { get; set; }
+        public bool ShowOriginalURL => !string.IsNullOrEmpty(OriginalURL);
+
+
+        public void OnGet(string statusCode = "500")
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            Referrer = HttpContext.Request.Headers["referer"];
+            ErrorStatusCode = statusCode;
+
+            // Do NOT expose sensitive error information directly to the client.
+            #region snippet_ExceptionHandlerPathFeature
+            var exceptionHandlerPathFeature =
+                HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+            {
+                ExceptionMessage += " - FileNotFoundException";
+            }
+            #endregion
+
+            #region snippet_StatusCodeReExecute
+            var statusCodeReExecuteFeature = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            if (statusCodeReExecuteFeature != null)
+            {
+                OriginalURL =
+                    statusCodeReExecuteFeature.OriginalPathBase
+                    + statusCodeReExecuteFeature.OriginalPath
+                    + statusCodeReExecuteFeature.OriginalQueryString;
+            }
+            #endregion
+
+        }
+    }
+}
+}
